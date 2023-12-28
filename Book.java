a3fd0ec3a7b9d98b241f5f8af3d83bbd7ff94384
Dylan <152401492+chops-07-chum@users.noreply.github.com>
2023-12-28 18:59:34 +0600

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,15 @@
+// Book.java
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author;
+    }
+}
